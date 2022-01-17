2366dc6e3ef557b980560780b559010eeb5641b3
Lucas Bargas <lucas.bargasoficial@gmail.com>
2022-01-17 09:06:57 -0300

    first commit

---
+++ html_e_sass_4/assets/js/app.js
@@ -0,0 +1,39 @@
+class Modal {
+  constructor() {
+    this.modal = document.querySelector('.jsModal');
+    this.classActive = 'active';
+    this.event = ['touchstart', 'click']
+    document.body.style.overflow = 'hidden';
+  }
+
+  hiddenModal() {
+    this.modal.classList.remove(this.classActive);
+    document.body.style.overflow = 'auto';
+  }
+
+  addEvent() {
+    this.event.forEach(userEvent => {
+      document.addEventListener(userEvent, e => {
+        const el = e.target;
+  
+        if (el.classList.contains(this.classActive)) {
+          this.hiddenModal();
+  
+        } else if (el.classList.contains('jsCloseButton')) {
+          this.hiddenModal();
+  
+        } else if (el.classList.contains('jsModalButton')) {
+          this.hiddenModal();
+        }
+      })
+    })
+  }
+
+  init() {
+    this.addEvent();
+    return this;
+  }
+}
+
+const modal = new Modal();
+modal.init();
